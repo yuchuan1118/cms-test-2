f87fbe1bdb383df704d19d61d6deaf2e1c00a2b6
yuchuan1118 <yuchuan1118@gmail.com>
2018-06-09 20:23:02 +0800

    code migration

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,14 @@
 import { combineReducers } from 'redux';
+import CoursesReducer from './reducer_courses';
+import ReceiptsReducer from './reducer_receipts';
+import UsersReducer from './reducer_users';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  courses: CoursesReducer,
+  receipts: ReceiptsReducer,
+  users: UsersReducer,
+  form: formReducer
 });
 
 export default rootReducer;
